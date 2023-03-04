fa546808ae5cb24c1f5ce5c72172be8c044e32e7
Teggenberg <tdeggenberger@student.fullsail.edu>
2023-03-04 09:44:16 -0600

    google sign in working?

--- app/src/main/java/com/example/narddogsinventory/MainActivity.kt
+++ app/src/main/java/com/example/narddogsinventory/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.example.narddogsinventory
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.content.Intent
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 
 private const val TAG = "MainActivity"
@@ -13,6 +24,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var register : Button
     private lateinit var login : Button
+    private lateinit var auth : FirebaseAuth
+    private lateinit var googleSignInClient : GoogleSignInClient
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,18 +35,87 @@ class MainActivity : AppCompatActivity() {
         register = findViewById(R.id.registerButton)
         login = findViewById(R.id.loginButton)
 
+        auth =  FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("890851707023-u4igendqs168g29ift6gnkkb4plkeuur.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
         register.setOnClickListener {
             val intentR = Intent(this@MainActivity, RegisterActivity::class.java)
             startActivity(intentR)
             finish()
         }
 
-        login.setOnClickListener{
-            val intentL = Intent(this@MainActivity, LoginActivity::class.java)
-            startActivity(intentL)
-            finish()
+        /*login.setOnClickListener{
+            signInGoogle()
+        }*/
+        findViewById<Button>(R.id.loginButton).setOnClickListener{
+            signInGoogle()
         }
 
 
+
+
     }
+
+    private fun signInGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+            }
+
+
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+
+        if(task.isSuccessful){
+
+            val account : GoogleSignInAccount? = task.result
+            if(account != null){
+                updateUI(account)
+            }
+
+        }
+
+        else{
+            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener{
+
+            if(it.isSuccessful){
+
+                val intentL = Intent(this, LoginActivity::class.java)
+                intentL.putExtra("email", account.email)
+                intentL.putExtra("name", account.displayName)
+                startActivity(intentL)
+
+
+
+            }
+            else{
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+
+
+    }
+
 }
--- app/src/main/java/com/example/narddogsinventory/RegisterActivity.kt
+++ app/src/main/java/com/example/narddogsinventory/RegisterActivity.kt
@@ -7,16 +7,16 @@ import android.widget.EditText
 
 class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var email : EditText
-    private lateinit var password : EditText
-    private lateinit var create : Button
+    private lateinit var email : EditText     //field to enter email
+    private lateinit var password : EditText  //field to enter password
+    private lateinit var create : Button      //button to create new account
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        email = findViewById(R.id.etRegEmail)
+        email = findViewById(R.id.etRegEmail)              //variables linked to xml objects
         password = findViewById(R.id.etRegPassword)
         create = findViewById(R.id.createAccountButton)
 
