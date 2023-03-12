c0c7892035bab054dd8a59fbe44523c61fe37770
Teggenberg <tdeggenberger@student.fullsail.edu>
2023-03-12 12:41:13 -0500

    comments and code clean up

--- app/src/main/java/com/example/narddogsinventory/GoogleReg.kt
+++ app/src/main/java/com/example/narddogsinventory/GoogleReg.kt
@@ -21,11 +21,11 @@ private const val TAG = "GoogleReg"
 
 class GoogleReg : AppCompatActivity() {
 
-    private lateinit var emailTV : TextView
+    //references for editTexts and button
     private lateinit var auth : FirebaseAuth
     private lateinit var register : Button
     private lateinit var email : String
-    //private  var uselessList : ArrayList<NewID> = TODO()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,11 +33,12 @@ class GoogleReg : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
-        email = intent.getStringExtra("email").toString()
-        register = findViewById<Button>(R.id.registerGoogleButton)
+        email = intent.getStringExtra("email").toString()     //display email in tv pulled form Google
+        register = findViewById<Button>(R.id.registerGoogleButton)  //button to register new account
 
-        findViewById<TextView>(R.id.tvEmail).text = "$email"
+        findViewById<TextView>(R.id.tvEmail).text = "$email"        // set tv to google address
 
+        //when Register button is clicked
         register.setOnClickListener{
             val first = findViewById<EditText>(R.id.etFirstName).text.toString()
             val last =  findViewById<EditText>(R.id.etLastName).text.toString()
@@ -47,15 +48,17 @@ class GoogleReg : AppCompatActivity() {
             if(fieldsComplete(first, last, password, passwordC)){  //validate that all fields are filled
 
                 if(password == passwordC){   //validate that passwords both match
-                    Toast.makeText(this, "Hello", Toast.LENGTH_SHORT)
-                    generateUserID()
-                    updateGlobals()
+
+                    generateUserID()  //create new account, user ID assigned from globals collection
+                    updateGlobals()   //totalUsers and UserID index updated in globals collection
                 }
                 else{
+                    //password fields do not match
                     Toast.makeText(this, "passwords do not match", Toast.LENGTH_SHORT).show()
                 }
             }
             else{
+                //empty fields remain
                 Toast.makeText(this, "credential fields incomplete", Toast.LENGTH_SHORT).show()
             }
         }
@@ -63,7 +66,7 @@ class GoogleReg : AppCompatActivity() {
 
     private fun passwordMatch(password: String, passwordC: String): Boolean {
 
-        if(password == passwordC){
+        if(password == passwordC){ //check string in each et to make sure they are equal
             return true
         }
         return false
@@ -71,6 +74,7 @@ class GoogleReg : AppCompatActivity() {
 
     private fun fieldsComplete(first: String, last: String, password: String, passwordC: String): Boolean {
 
+        //check each et to make sure there are no empty fields
         if(first.isNullOrEmpty() || last.isNullOrEmpty() || password.isNullOrEmpty() || passwordC.isNullOrEmpty()){
             return false
         }
@@ -80,70 +84,79 @@ class GoogleReg : AppCompatActivity() {
 
     private fun addUser(email: String, fName: String, lName: String, password: String, userId : Long?) {
 
-
+        //instantiate firestore access
         val db = FirebaseFirestore.getInstance()
 
+        //custom object to hold data being written to db
         val newUser = EntryUser(0, fName, lName, password, userId)
 
+        //pass object into collection to create new doc in user collection
         db.collection("Users").document(email).set(newUser)
-
-
 
     }
 
     private fun generateUserID(){
+
+        //read in values in editTexts for user data fields
         val first = findViewById<EditText>(R.id.etFirstName).text.toString()
         val last =  findViewById<EditText>(R.id.etLastName).text.toString()
         val password = findViewById<EditText>(R.id.etPassword).text.toString()
 
-
+        //variable to hold userID accessed from globals table
         var returnID : Long? = null
+
+
+        //instantiate db access, and target specific document
         val global = FirebaseFirestore.getInstance()
         val docRef = global.collection("globals").document("rJd01G5J8l1BTkwyo101")
 
+
+        //document retrieved from globals to access user ID for new user
         docRef.get().addOnSuccessListener { documentsnapshot ->
 
-            val newUser = documentsnapshot.toObject<NewID>()
-            Log.i(TAG, newUser?.userID.toString())
-            returnID = newUser?.userID
+            val newUser = documentsnapshot.toObject<NewID>() //document captured into object
+            Log.i(TAG, newUser?.userID.toString()) //logkat to verify proper function
+            returnID = newUser?.userID //assign userID to variable
+
+            //call addUser with all data passed for new account
             addUser(email, first, last, password, returnID)
-
         }
-
-
 
     }
 
     private fun updateGlobals(){
 
+        //db access to global document
         val global = FirebaseFirestore.getInstance()
         val docRef = global.collection("globals").document("rJd01G5J8l1BTkwyo101")
 
         docRef.get().addOnSuccessListener { documentsnapshot ->
 
+            //assign document to object for data access
             val updateID = documentsnapshot.toObject<NewID>()
             Log.i(TAG, updateID?.userID.toString())
 
-
-
-
+            //check to make sure document is not null
             if (updateID != null) {
+
+                //increment global userID for next assignment
                 updateID?.userID = updateID?.userID?.plus(1)
 
+                //check to see if total users is null, increment value
                 if(updateID?.totalUsers != null) {
                     updateID.totalUsers = updateID.totalUsers?.plus(1)
                 }
 
+                //if null assign 1 as value
                 else{
                     updateID?.userID = 1}
 
+                //update document with new values
                 global.collection("globals").document("rJd01G5J8l1BTkwyo101")
                     .set(updateID)
             }
 
-
         }
-
-
+        
     }
 }
