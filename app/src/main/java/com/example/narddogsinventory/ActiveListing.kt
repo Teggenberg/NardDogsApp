f7b6e48d61cef89b27f5ae64f49acd2088c2a846
Teggenberg <tdeggenberger@student.fullsail.edu>
2023-03-14 19:29:10 -0500

    recycler prog

--- app/src/main/java/com/example/narddogsinventory/ActiveListing.kt
+++ app/src/main/java/com/example/narddogsinventory/ActiveListing.kt
@@ -1,4 +1,14 @@
 package com.example.narddogsinventory
 
-data class AvtiveListing() {
+data class ActiveListing(
+    val age: Int? = 0,
+    val brand : String? = null,
+    val category : String? = null,
+    val cost : Float? = null,
+    val estRetail : Float? = null,
+    val imageURL : String? = null,
+    val itemDesc : String? = null,
+    val itemID : Long? = null,
+    val notes : String? = null,
+    val user : Long? = null) {
 }
