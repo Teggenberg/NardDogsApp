d07575bf7ab637ae8c442f87434e6f929e4eaf92
Teggenberg <tdeggenberger@student.fullsail.edu>
2023-03-19 20:23:25 -0500

    almost ready for testing

--- app/src/main/java/com/example/narddogsinventory/ViewInventory.kt
+++ app/src/main/java/com/example/narddogsinventory/ViewInventory.kt
@@ -2,10 +2,34 @@ package com.example.narddogsinventory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+
+
+
 
 class ViewInventory : AppCompatActivity() {
+
+    private lateinit var dbRef : FirebaseFirestore
+    private lateinit var inventoryRecyclerView: RecyclerView
+    private lateinit var itemAdapter : ItemAdapter
+    private lateinit var itemList : ArrayList<ActiveListing>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_inventory)
+
+        inventoryRecyclerView = findViewById(R.id.inventoryList)
+        inventoryRecyclerView.layoutManager = LinearLayoutManager(this)
+        inventoryRecyclerView.setHasFixedSize(true)
+
+        itemList = arrayListOf()
+
+        itemAdapter = ItemAdapter(itemList)
+
+        inventoryRecyclerView.adapter = itemAdapter
     }
+
+
 }
