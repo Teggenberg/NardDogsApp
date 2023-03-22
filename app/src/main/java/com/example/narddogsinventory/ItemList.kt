96c9b9db80e2457b2c1bb5a21abe534b400684fa
James Kuzas <JRKuzas@student.fullsail.edu>
2023-03-21 19:38:12 -0500

    Camera without save picture

--- app/src/main/java/com/example/narddogsinventory/ItemList.kt
+++ app/src/main/java/com/example/narddogsinventory/ItemList.kt
@@ -1,38 +1,57 @@
 package com.example.narddogsinventory
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.DropBoxManager
 import android.provider.MediaStore
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import androidx.core.content.ContentProviderCompat.requireContext
+import com.google.android.material.navigation.NavigationBarView
 
 class ItemList : AppCompatActivity() {
 
-
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        /*val coItems = arrayOf("Option1","Option2")
-        val adapter = ArrayAdapter(this,R.layout.dropdown_text,coItems)
-        val autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.etDropDownCon)
-        autoCompleteTextView.setAdapter(adapter)*/
-
-
         setContentView(R.layout.activity_item_list)
-    }
 
-    private fun useCamera(){
 
-        findViewById<Button>(R.id.opCamera).setOnClickListener {
-            if (it.callOnClick()) {
-                var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                startActivity(intent)
-            }
+//      CONDITIONS
+        // get reference to the string array that we just created
+        val con = resources.getStringArray(R.array.conditions)
+        // create an array adapter and pass the required parameter
+        // in our case pass the context, drop down layout , and array.
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_text, con)
+        // get reference to the autocomplete text view
+        val autocompleteTV = findViewById<AutoCompleteTextView>(R.id.etDropDownCon)
+        // set adapter to the autocomplete tv to the arrayAdapter
+        autocompleteTV.setAdapter(arrayAdapter)
+
+//      CATEGORIES
+        // get reference to the string array that we just created
+        val cat = resources.getStringArray(R.array.categories)
+        // create an array adapter and pass the required parameter
+        // in our case pass the context, drop down layout , and array.
+        val adapter = ArrayAdapter(this, R.layout.dropdown_text, cat)
+        // get reference to the autocomplete text view
+        val auto = findViewById<AutoCompleteTextView>(R.id.etDropDownBox)
+        // set adapter to the autocomplete tv to the arrayAdapter
+        auto.setAdapter(adapter)
+
+
+//      Open CAMERA
+
+        findViewById<Button>(R.id.opCamera).setOnClickListener{
+
+            var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
         }
 
     }
+
 
 }
