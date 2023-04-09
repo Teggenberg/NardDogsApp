8475a96d0573f81f117f57979132de335b63547e
Teggenberg <tdeggenberger@student.fullsail.edu>
2023-04-09 10:51:49 -0500

    ready to add fields for user

--- app/src/main/java/com/example/narddogsinventory/EntryUser.kt
+++ app/src/main/java/com/example/narddogsinventory/EntryUser.kt
@@ -11,38 +11,7 @@ data class EntryUser(
     val password : String? = null,
     val userID : Long? = null
 ) : Parcelable {
-//    constructor(parcel: Parcel) : this(
-//        parcel.readValue(Long::class.java.classLoader) as? Long,
-//        parcel.readString(),
-//        parcel.readString(),
-//        parcel.readString(),
-//        parcel.readString(),
-//        parcel.readValue(Long::class.java.classLoader) as? Long
-//    ) {
-//    }
-//
-//    override fun writeToParcel(parcel: Parcel, flags: Int) {
-//        parcel.writeValue(currentListing)
-//        parcel.writeValue(email)
-//        parcel.writeString(firstName)
-//        parcel.writeString(lastName)
-//        parcel.writeString(password)
-//        parcel.writeValue(userID)
-//    }
-//
-//    override fun describeContents(): Int {
-//        return 0
-//    }
-//
-//    companion object CREATOR : Parcelable.Creator<EntryUser> {
-//        override fun createFromParcel(parcel: Parcel): EntryUser {
-//            return EntryUser(parcel)
-//        }
-//
-//        override fun newArray(size: Int): Array<EntryUser?> {
-//            return arrayOfNulls(size)
-//        }
-//    }
+
     constructor(parcel: Parcel) : this(
         parcel.readValue(Long::class.java.classLoader) as? Long,
         parcel.readString(),
