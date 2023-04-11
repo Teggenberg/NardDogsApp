ec0476afd8b8455463306af40e10ed170b4e3f58
Teggenberg <tdeggenberger@student.fullsail.edu>
2023-04-11 11:39:47 -0500

    soldListing class, widgets on View Item

--- app/src/main/java/com/example/narddogsinventory/SoldListing.kt
+++ app/src/main/java/com/example/narddogsinventory/SoldListing.kt
@@ -1,4 +1,16 @@
 package com.example.narddogsinventory
 
-class SoldListing {
-}
+data class SoldListing (
+    val age: Int? = null,
+    val brand : String? = null,
+    val category : String? = null,
+    val condition : Int? = null,
+    val cost : Float? = null,
+    val finalRetail : Float? = null,
+    val imageURL : String? = null,
+    val itemDesc : String? = null,
+    val itemID : Long? = null,
+    val notes : String? = null,
+    val soldDate : String? = null,
+    val user : Long? = null
+    )
