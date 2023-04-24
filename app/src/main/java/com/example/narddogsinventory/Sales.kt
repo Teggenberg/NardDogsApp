7e53cfb6e53fbe117c2cfa068b567ac734903b32
Teggenberg <tdeggenberger@student.fullsail.edu>
2023-04-24 08:46:12 -0500

    comments

--- app/src/main/java/com/example/narddogsinventory/Sales.kt
+++ app/src/main/java/com/example/narddogsinventory/Sales.kt
@@ -17,6 +17,7 @@ import java.time.LocalDateTime
 
 class Sales : AppCompatActivity() {
 
+    //references to textViews for data display
     private lateinit var sales : TextView
     private lateinit var catOne : TextView
     private lateinit var catTwo : TextView
@@ -31,9 +32,11 @@ class Sales : AppCompatActivity() {
     private lateinit var bNav : NavigationBarView
     private var currentUser : EntryUser? = null
 
+    //lists to capture the display data
     private lateinit var salesList : ArrayList<SoldListing>
     private lateinit var catList : ArrayList<SalesCategory>
 
+    //to toggle filters for dates. 'all time' default as true for no date filtering
     private var allTime = true
     private var pastThirty = false
     private var pastNinety = false
@@ -47,27 +50,22 @@ class Sales : AppCompatActivity() {
         setContentView(R.layout.activity_sales)
 
 
-
+        //initialize user from previous activity
         currentUser = intent.getParcelableExtra("currentUser", EntryUser::class.java)
 
+        //initialize lists
         catList = arrayListOf()
         salesList = arrayListOf()
 
-//        catList.add(SalesCategory("Electronics", 0, 0f, 0f, 0f, 0f))
-//        catList.add(SalesCategory("Apparel",0,0f,0f,0f,0f))
-//        catList.add(SalesCategory("Media",0,0f,0f,0f,0f))
-//        catList.add(SalesCategory("Furniture/Appliances",0,0f,0f,0f,0f))
-//        catList.add(SalesCategory("Collectibles",0,0f,0f,0f,0f))
-//        catList.add(SalesCategory("Other",0,0f,0f,0f,0f))
-
+        //populate lists defaults to all time, no filters
         buildLists()
 
 
-
+        //assign bottom nav, set item to current activity
         bNav = findViewById(R.id.bottomNav)
-
         bNav.selectedItemId = R.id.Sales
 
+        //assign textView references
         sales = findViewById(R.id.tvTotalRev)
         catOne = findViewById(R.id.tvCatOne)
         catTwo = findViewById(R.id.tvCatTwo)
@@ -76,24 +74,7 @@ class Sales : AppCompatActivity() {
         catFive = findViewById(R.id.tvCatFive)
         catSix = findViewById(R.id.tvCatSix)
 
-
-
-
-
-
-
-
-
-        val origin = LocalDateTime.parse("2023-01-01T20:00:00.0000")
-        val current = LocalDateTime.now()
-
-        val days = Duration.between(origin,current).toDays().toString()
-
-
-
-
-
-
+        //listener for user click on nav bar
         bNav.setOnItemSelectedListener {
 
             when(it.itemId) {
@@ -142,53 +123,53 @@ class Sales : AppCompatActivity() {
 
 
         // ALL Time, Month, and 90 days radio button code
-
         val radioGroup = findViewById<RadioGroup>(R.id.radio_group)
-        val weekButton = findViewById<RadioButton>(R.id.All_Time_button)
+        val allButton = findViewById<RadioButton>(R.id.All_Time_button)
         val monthButton = findViewById<RadioButton>(R.id.month_button)
         val yearButton = findViewById<RadioButton>(R.id.three_Months_button)
 
 
-
+        //listener for date filter buttons
         radioGroup.setOnCheckedChangeListener { _, checkedId ->
             when (checkedId) {
                 R.id.All_Time_button -> {
+
+                    //reassign filters for proper date data
                     allTime = true
                     pastNinety = false
                     pastThirty = false
+                    //clear current data for fresh start
                     catList.clear()
                     salesList.clear()
                     totalRev = 0f
+                    //rebuild lists with current filer selection
                     buildLists()
-                    Toast.makeText(this, "all time", Toast.LENGTH_SHORT).show()
-
-
                 }
                 R.id.month_button -> {
+
+                    //reassign filters for proper date data
                     allTime = false
                     pastNinety = false
                     pastThirty = true
+                    //clear current data for fresh start
                     catList.clear()
                     salesList.clear()
                     totalRev = 0f
+                    //rebuild lists with current filer selection
                     buildLists()
-                    Toast.makeText(this, "past 3 days", Toast.LENGTH_SHORT).show()
-
-
                 }
                 R.id.three_Months_button -> {
 
+                    //reassign filters for proper date data
                     allTime = false
                     pastNinety = true
                     pastThirty = false
+                    //clear current data for fresh start
                     catList.clear()
                     salesList.clear()
                     totalRev = 0f
+                    //rebuild lists with current filer selection
                     buildLists()
-
-                    Toast.makeText(this, pastThirty.toString(), Toast.LENGTH_SHORT).show()
-
-
                 }
             }
         }
@@ -197,6 +178,7 @@ class Sales : AppCompatActivity() {
 
     private fun buildLists() {
 
+        //populate category list with default categories, all values initialized to 0
         catList.add(SalesCategory("Electronics", 0, 0f, 0f, 0f, 0f))
         catList.add(SalesCategory("Apparel",0,0f,0f,0f,0f))
         catList.add(SalesCategory("Media",0,0f,0f,0f,0f))
@@ -204,6 +186,7 @@ class Sales : AppCompatActivity() {
         catList.add(SalesCategory("Collectibles",0,0f,0f,0f,0f))
         catList.add(SalesCategory("Other",0,0f,0f,0f,0f))
 
+        //reference and access to database
         db = FirebaseFirestore.getInstance()
         db.collection("soldListings").whereEqualTo("user", currentUser?.userID).
         addSnapshotListener(object : EventListener<QuerySnapshot>{
@@ -213,44 +196,29 @@ class Sales : AppCompatActivity() {
                 error: FirebaseFirestoreException?
             ) {
 
+                //if no connection to db is established
                 if(error != null){
                     Log.e("Firestore Error", error.message.toString() )
-
+                    Toast.makeText(this, "cannot contact server", Toast.LENGTH_SHORT).show()
                     return
                 }
 
+                //go through each document in query
                 for(dc : DocumentChange in value?.documentChanges!!){
 
-
+                    //add new item to sales list while looping through query
                     if(dc.type == DocumentChange.Type.ADDED){
                         Log.e("Document :", dc.toString() )
-
                         salesList.add(dc.document.toObject(SoldListing::class.java))
-
-
                     }
                 }
 
+                //use the items in sales list to populate data in category list
                 buildCategories()
                 sales.text = "Total Sales : $" + "%.2f".format(totalRev)
-//                catOne.text = catList[0].category + " : " + catList[0].totDollars.toString()
-//                catTwo.text = catList[1].category + " : " + catList[1].totDollars.toString()
-//                catThree.text = catList[2].category + " : " + catList[2].totDollars.toString()
-//                catFour.text = catList[3].category + " : " + catList[3].totDollars.toString()
-//                catFive.text = catList[4].category + " : " + catList[4].totDollars.toString()
-//                catSix.text = catList[5].category + " : " + catList[5].totDollars.toString()
-                Log.e("Category :", catList[0].totDollars.toString() )
-
-
 
             }
-
-
         })
-
-        //Toast.makeText( this, "total items ${salesList.size}", Toast.LENGTH_LONG).show()
-
-        //buildCategories()
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
